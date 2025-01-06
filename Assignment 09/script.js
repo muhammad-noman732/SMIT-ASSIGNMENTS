1654f728bff519891f3277aa72fcf3516bb6b69a
muhammad-noman732 <iasknoman156@gmail.com>
2025-01-06 06:18:13 -0800

    Updated Assignment 02

---
+++ Assignment 09/script.js
@@ -0,0 +1,8 @@
+//                                  List of loop programming exercises
+
+
+// 1. Write a js program to print all natural numbers from 1 to n. - using while loop
+
+var num = prompt("Enter a Number to which print ");
+let  = 1 ;
+while()
